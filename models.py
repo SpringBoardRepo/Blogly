5deb3db49feecb4d2ea05035c9b66c0f4e308f79
poorvarathore05 <poorvarathore05@gmail.com>
2021-05-25 16:12:45 -0700

    adding models

--- models.py
+++ models.py
@@ -1,1 +1,14 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connectdb(app):
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+
+    __tablename__ = 'users'
