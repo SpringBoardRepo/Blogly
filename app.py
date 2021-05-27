3b3097ec0850be641ef1e91e4fdf829b26352914
poorvarathore05 <poorvarathore05@gmail.com>
2021-05-26 18:04:28 -0700

    added home route

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 """Blogly application."""
 
 from flask import Flask
-from models import db, connect_db
+from flask.templating import render_template
+from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/blogly?user=postgres&password=postgresql'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'flaskblogly'
 
 connect_db(app)
-db.create_all()
+
+
+@app.route('/')
+def home_page():
+    """"list all users"""
+    users = User.query.all()
+    return render_template('user.html', users=users)
+
+
+@app.route('/<int:user_id>')
+def detail_page(user_id):
+    """show user details"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('details.html', user=user)
